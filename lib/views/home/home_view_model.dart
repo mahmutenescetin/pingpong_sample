c2b1c70f0227dc1759f7768eefc9959c4a067b2b
otoplus-example <mahmutenescetin@gmail.com>
2025-07-22 12:39:37 +0300

    firebase login added project

--- lib/views/home/home_view_model.dart
+++ lib/views/home/home_view_model.dart
@@ -1,5 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewModel extends ChangeNotifier {
-  // View model logic buraya gelecek
+  final auth = FirebaseAuth.instance;
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  Future<void> register(BuildContext context) async {
+    try {
+      await auth.createUserWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('Kayıt başarılı')));
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> login(BuildContext context) async {
+    try {
+      await auth.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('Giriş başarılı')));
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 }
